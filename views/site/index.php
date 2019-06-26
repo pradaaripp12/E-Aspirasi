269be6ec1fe1694f179a1636f5ff99f0d23552fb
fauzivy <fauzivyreggi@gmail.com>
2019-06-26 09:44:55 +0700

    Ganti kalimat sambutan

--- views/site/index.php
+++ views/site/index.php
@@ -4,22 +4,33 @@
 $this->title = 'E-Aspirasi';
 ?>
 <div class="site-index">
-
     
-
     <div class="body-content">
 
         <div class="row">
             <?php echo \Yii::$app->view->renderFile('@app/views/layouts/sidebar.php'); ?>
             <div class="col-lg-8">
-                <h2>Heading</h2>
+                <?php
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                $is_admin = Yii::$app->user->identity->is_admin;
+                if($is_admin){
+                    echo '<h2>Selamat Datang!</h2>
+                    <p>Segera cek data update aspirasi masyarakat dan berikan evaluasi.
+                    </p>
+                    <p><a class="btn btn-default" href="">Cek Aspirasi &raquo;</a></p>';
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                }else{
+                    echo '<h2>Selamat Datang!</h2>
+
+                    <p>Warga negara yang hidup di Republik Indonesia memiliki kapasitas untuk menyuarakan pendapatnya secara lugas terhadap sistem dan 
+                    keberlangsungan negara. Sebagai penduduk wilayah Surakarta, kami memberikan fasilitas penyampaian pendapat untuk masyarakat di wilayah
+                    Surakarta. Dengan adanya situs ini, kami memfasilitasi warga untuk mendukung tercapainya negara yang lebih baik melalui evaluasi tiap 
+                    insfrastruktur, layanan, dan masalah keamanan.
+                    </p>
+
+                    <p><a class="btn btn-default" href="">Sampaikan Aspirasi &raquo;</a></p>';
+                }
+                ?>
             </div>
             
         </div>
