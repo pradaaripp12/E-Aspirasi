d4f5dfc6f12c67e1aca760d53612a0a94e39463c
yaumiaz <yaumiaziizah@student.uns.ac.id>
2019-06-30 23:16:26 +0700

    gatau

--- views/aspiration/_form.php
+++ views/aspiration/_form.php
@@ -2,17 +2,30 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\base\DynamicModel;
 use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Aspiration */
 /* @var $form yii\widgets\ActiveForm */
+
+$dynamic_model = new DynamicModel(['jenis_aspirasi']);
+$dynamic_model->addRule('jenis_aspirasi', 'required');
 ?>
 
 <div class="aspiration-form">
 
     <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($dynamic_model, 'jenis_aspirasi')->dropDownList(
+        [
+            'A' => 'Infrastruktur',
+            'B' => 'Keamanan',
+        ],
+        [
+            'prompt' => 'Pilih jenis aspirasi'
+        ]
+    ) ?>
 
     <?= $form->field($model, 'tanggal')->widget(DatePicker::className(),[
         'options' => ['placeholder' => 'Masukkan tanggal aspirasi'],
@@ -22,7 +35,7 @@ use kartik\date\DatePicker;
         ]
     ]); ?>
 
-    <?= $form->field($model, 'id_anggota')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'id_anggota')->textInput(['value' => Yii::$app->user->identity->id_anggota]) ?>
 
     <?= $form->field($model, 'id_wilayah')->textInput() ?>
 
@@ -30,9 +43,9 @@ use kartik\date\DatePicker;
 
     <?= $form->field($model, 'judul')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'deskripsi')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'deskripsi')->textArea() ?>
 
-    <?= $form->field($model, 'tanggapan')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'tanggapan')->label(false)->hiddenInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
--- views/region/create.php
+++ views/region/create.php
@@ -6,11 +6,12 @@ use yii\helpers\Html;
 /* @var $model app\models\Region */
 
 $this->title = 'Create Region';
-$this->params['breadcrumbs'][] = ['label' => 'Regions', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+// $this->params['breadcrumbs'][] = ['label' => 'Regions', 'url' => ['index']];
+// $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="region-create">
-
+<?php echo \Yii::$app->view->renderFile('@app/views/layouts/sidebar.php'); ?>
+    <div class="col-lg-8">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
